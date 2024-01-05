0bf56b95bb0673ad99829a794caa48ec484e3e62
RomeroGabriel <gabrielromerods@hotmail.com>
2024-01-05 17:41:56 -0300

    add internal db and entity

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/RomeroGabriel/go-gRPC
 
 go 1.21.1
+
+require (
+	github.com/RomeroGabriel/go-graphQL v0.0.0-20240104215342-6079364172f5
+	github.com/google/uuid v1.5.0
+)
---
+++ internal/db/category_db.go
@@ -0,0 +1,72 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"log"
+
+	entity "github.com/RomeroGabriel/go-gRPC/internal/entity"
+	"github.com/google/uuid"
+)
+
+type CategoryDB struct {
+	db *sql.DB
+}
+
+func NewCategory(db *sql.DB) *CategoryDB {
+	sqlStmt := `
+	CREATE TABLE IF NOT EXISTS Categories 
+		(id TEXT NOT NULL, name TEXT NOT NULL, description TEXT NOT NULL);
+	`
+	ctx := context.Background()
+	conn, err := db.Conn(ctx)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return nil
+	}
+	defer conn.Close()
+	_, err = conn.ExecContext(ctx, sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return nil
+	}
+	return &CategoryDB{db: db}
+}
+
+func (c *CategoryDB) Create(name string, description string) (entity.Category, error) {
+	id := uuid.New().String()
+	_, err := c.db.Exec("INSERT INTO Categories (id, name, description) VALUES ($1, $2, $3)",
+		id, name, description)
+
+	if err != nil {
+		return entity.Category{}, err
+	}
+	return entity.Category{Id: id, Name: name, Description: description}, nil
+}
+
+func (c *CategoryDB) FindAll() ([]entity.Category, error) {
+	rows, err := c.db.Query("SELECT id, name, description FROM Categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	categories := []entity.Category{}
+	for rows.Next() {
+		var id, name, description string
+		if err := rows.Scan(&id, &name, &description); err != nil {
+			return nil, err
+		}
+		categories = append(categories, entity.Category{Id: id, Name: name, Description: description})
+	}
+	return categories, nil
+}
+
+func (c *CategoryDB) FindAllByCourseId(courseId string) (entity.Category, error) {
+	var id, name, description string
+	err := c.db.QueryRow("SELECT c.id, c.name, c.description FROM categories c JOIN courses co ON c.id = co.categoryId WHERE co.id = $1", courseId).
+		Scan(&id, &name, &description)
+	if err != nil {
+		return entity.Category{}, err
+	}
+	return entity.Category{Id: id, Name: name, Description: description}, nil
+}
