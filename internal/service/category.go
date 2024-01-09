856e3a93c3a4f741874427f2c34a11415b3614c3
RomeroGabriel <gabrielromerods@hotmail.com>
2024-01-09 10:57:05 -0300

    add  list categories

--- internal/service/category.go
+++ internal/service/category.go
@@ -33,3 +33,21 @@ func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCateg
 		Category: categoryResponse,
 	}, err
 }
+
+func (c *CategoryService) ListCategories(context.Context, *pb.Blank) (*pb.CategoryList, error) {
+	categories, err := c.CatetgoryDb.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var categoriesResponse []*pb.Category
+	for _, category := range categories {
+		categoryResponse := &pb.Category{
+			Id:          category.Id,
+			Name:        category.Name,
+			Description: category.Description,
+		}
+		categoriesResponse = append(categoriesResponse, categoryResponse)
+	}
+	return &pb.CategoryList{Categories: categoriesResponse}, nil
+}
