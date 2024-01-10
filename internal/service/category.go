5ffd9934fe3983abce5dc414e196536ee2adc354
RomeroGabriel <gabrielromerods@hotmail.com>
2024-01-10 11:50:02 -0300

    add CreateCategoryStreamBidirectional

--- internal/service/category.go
+++ internal/service/category.go
@@ -88,3 +88,27 @@ func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateC
 		})
 	}
 }
+
+func (c *CategoryService) CreateCategoryStreamBidirectional(stream pb.CategoryService_CreateCategoryStreamBidirectionalServer) error {
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		categoryResult, err := c.CatetgoryDb.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+		err = stream.Send(&pb.Category{
+			Id:          categoryResult.Id,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+		if err != nil {
+			return err
+		}
+	}
+}
